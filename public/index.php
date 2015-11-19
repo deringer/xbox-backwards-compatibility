23bb8e22e49d4020eacfb573db442d6fec1a5a8f
Michael Dyrynda <michaeld@hostworks.com.au>
2015-11-20 10:26:37 +1030

    Add bootstrap styles to index page

--- public/index.php
+++ public/index.php
@@ -9,21 +9,37 @@ $cache = new GamesCache(getenv('XBOX_BC_GAME_CACHE'));
 $lastUpdated       = $cache->lastUpdated()->setTimezone(new \DateTimeZone(getenv('DEFAULT_TIMEZONE')));
 $recentLastUpdated = $cache->recentLastUpdated()->setTimezone(new \DateTimeZone(getenv('DEFAULT_TIMEZONE')));
 
-print '<!doctype html><html lang="en"><meta charset="utf-8">';
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Xbox One Backwards Compatibility</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <h1>Xbox One Backwards Compatibility titles <small>Last updated <?= $lastUpdated ? $lastUpdated->format('d/m/Y H:i:s') : 'never' ?></small></h1>
 
-printf(
-    '<h1>Xbox One Backwards Compatibility titles <small>Last updated %s</small></h1>',
-    $lastUpdated ? $lastUpdated->format('d/m/Y H:i:s') : 'never'
-);
-
-print '<table>';
-foreach (array_chunk($cache->games(), 3) as $chunk) {
-    print '<tr>';
-    foreach ($chunk as $game) {
-        printf('<td%s>%s</td>', $cache->isRecent($game) ? ' style="background-color: #a7d490;"' : null, $game);
-    }
-    print '</tr>';
-}
-print '</table>';
-printf('<em style="background-color: #a7d490;">Recent titles last updated %s</em>', $recentLastUpdated->format('d/m/Y H:i:s'));
-print '</html>';
+        <table class="table table-bordered">
+        <col style="width: 25%;" />
+        <col style="width: 25%;" />
+        <col style="width: 25%;" />
+        <col style="width: 25%;" />
+        <?php foreach (array_chunk($cache->games(), 4) as $chunk): ?>
+            <tr>
+                <?php foreach ($chunk as $game): ?>
+                    <td <?= $cache->isRecent($game) ? 'class="success"' : null ?>><?= $game ?></td>
+                <?php endforeach; ?>
+            </tr>
+        <?php endforeach; ?>
+        <tr>
+            <td class="success" colspan="4">
+                Recent titles last updated <?= $recentLastUpdated ? $recentLastUpdated->format('d/m/Y H:i:s') : 'never' ?>
+            </td>
+        </tr>
+        </table>
+    </div>
+</html>
