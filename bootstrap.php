8e3f947da37ff3d69a716c4418af11377e6b6077
Michael Dyrynda <michaeld@hostworks.com.au>
2015-11-20 10:16:20 +1030

    Set the default timezone on bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -5,3 +5,5 @@ require dirname(__FILE__) . '/vendor/autoload.php';
 $dotenv = new Dotenv\Dotenv(dirname(__FILE__));
 $dotenv->load();
 $dotenv->required([ 'XBOX_SITE_ENDPOINT', ])->notEmpty();
+
+ini_set('date.timezone', getenv('DEFAULT_TIMEZONE'));
